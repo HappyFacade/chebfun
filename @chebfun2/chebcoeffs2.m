141ccb74be0fd19c95d7d9171757a630f1d3ed48
alex60gaibb <alex.townsend1987@gmail.com>
2019-08-19 08:27:41 -0400

    Make sure chebcoeffs2 can be supplied a discretization size.

--- @chebfun2/chebcoeffs2.m
+++ @chebfun2/chebcoeffs2.m
@@ -1,14 +1,19 @@
-function varargout = chebcoeffs2(f)
+function varargout = chebcoeffs2(f, m, n)
 %CHEBCOEFFS2    Bivariate Chebyshev coefficients
 %   X = CHEBCOEFFS2(F) returns the matrix of bivariate coefficients such that
-%       F= sum_{i=0}^{n-1} ( sum_{j=0}^{n-1} X(i+1,j+1) T_i(y) T_j(x) ).
+%       F = sum_{i=0}^{m-1} ( sum_{j=0}^{n-1} X(i+1,j+1) T_i(y) T_j(x) ),
+%   where m and n are the degree of F. 
 %
 %   [A, D, B] = CHEBCOEFFS2( f ) returns the same coefficients keeping them in
 %   low rank form, i.e., X = A * D * B'.
 %
+%   CHEBCOEFFS2(F, M, N) is the same as CHEBCOEFFS2(F) except the matrix of
+%   coefficients is returned as an MxN matrix. CHEBCOEFFS2(F, M) is the
+%   same as CHEBCOEFFS2(F, M, M).
+%
 % See also PLOTCOEFFS2, CHEBCOEFFS.
 
-% Copyright 2017 by The University of Oxford and The Chebfun Developers.
+% Copyright 2019 by The University of Oxford and The Chebfun Developers.
 % See http://www.chebfun.org/ for Chebfun information.
 
 if ( isempty(f) )
@@ -25,8 +30,18 @@ end
 [cols, d, rows] = cdr(f);
 
 % Get the coeffs of the rows and the columns:
-cols_coeffs = chebcoeffs(cols);
-rows_coeffs = chebcoeffs(rows);
+if ( nargin == 1 )
+    cols_coeffs = chebcoeffs(cols);
+    rows_coeffs = chebcoeffs(rows);
+elseif ( nargin == 2 )
+    cols_coeffs = chebcoeffs(cols, m);
+    rows_coeffs = chebcoeffs(rows, m);
+elseif ( nargin == 3 )
+    cols_coeffs = chebcoeffs(cols, m);
+    rows_coeffs = chebcoeffs(rows, n);
+else 
+    error('CHEBFUN2:CHEBCOEFFS2:ARGIN', 'Too many input arguments');
+end
 
 if ( nargout <= 1 )
     % Return the matrix of coefficients
--- tests/chebfun2/test_chebcoeffs2.m
+++ tests/chebfun2/test_chebcoeffs2.m
@@ -16,14 +16,20 @@ Tm = chebpoly(m);
 f = Tn * Tn' + Tm * Tn'; 
 X = chebcoeffs2( f );
 Exact = zeros(n+1); Exact(n+1,n+1) = 1; Exact(m+1,n+1) = 1; 
-pass(j) = norm( X - Exact ) < tol; j = j + 1; 
+pass(1) = norm( X - Exact ) < tol; 
+
+X = chebcoeffs2( f, 20, 30);
+Exact = zeros(20, 30); 
+Exact(n+1,n+1) = 1; Exact(m+1,n+1) = 1; 
+pass(2) = norm( X - Exact ) < tol; 
 
 %f = Tm * Tn';
 % check inverses
+Exact = Exact(1:n+1,1:n+1); 
 Z = chebfun2.vals2coeffs( chebpolyval2( f ) );
-pass(j) = norm( Z - Exact ) < tol; j = j + 1; 
+pass(3) = norm( Z - Exact ) < tol; 
 
 f = chebfun2( @(x,y) x + y );
-pass(j) = isequal( size(coeffs2(f,2,1)), [2 1] ); j = j + 1;
+pass(4) = isequal( size(coeffs2(f,2,1)), [2 1] ); 
 
 end
